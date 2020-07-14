81615d68cc1bf33f40ec23f90ba808fde547d2eb
PrabhShergill <PrabhShergill@github.com>
2020-07-14 09:05:23 -0400

    template complete

--- portfolio/main.js
+++ portfolio/main.js
@@ -3,4 +3,32 @@ $(document).ready(function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault()
+      
+        $('html, body').animate(
+          {
+            scrollTop: $($(this).attr('href')).offset().top,
+          },
+          2000
+        )
+    });
+
+    $("#up").click(function (){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1000,
+        once: true
+    });
 });
